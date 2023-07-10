08ef75dc2ebc80100e81ed4dafe391f510bbf920
skraatzgm <gokmert_han@hotmail.com>
2023-07-10 12:21:50 +0300

    Feat(ONB-101) - PostRepository-PostService-ServiceTests updated.

---
+++ web/build/resources/test/data/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, name, username, email, phone) VALUES (11, 'John Doe', 'johndoe', 'johndoe@example.com', '1234567890');
+INSERT INTO users (id, name, username, email, phone) VALUES (12, 'Jane Smith', 'janesmith', 'janesmith@example.com', '9876543210');
+INSERT INTO users (id, name, username, email, phone) VALUES (13, 'Alice Johnson', 'alicejohnson', 'alicejohnson@example.com', '5551234567');
+INSERT INTO users (id, name, username, email, phone) VALUES (14, 'Bob Williams', 'bobwilliams', 'bobwilliams@example.com', '9998887777');
---
+++ web/out/test/resources/data/posts.sql
@@ -0,0 +1,4 @@
+insert into posts(id, user_id, title, body) values (101, 1, 'title-1', 'body-1');
+insert into posts(id, user_id, title, body) values (102, 1, 'title-2', 'body-2');
+insert into posts(id, user_id, title, body) values (103, 2, 'title-3', 'body-3');
+insert into posts(id, user_id, title, body) values (104, 3, 'title-4', 'body-4');
