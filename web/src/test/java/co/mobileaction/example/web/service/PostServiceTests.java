7f209f9e5fb047e82ea9d74f0889762990788322
Sude Akarçay <akarcaysude2@gmail.com>
2023-08-12 12:15:56 +0300

    test(ONB-101):deleteAllPostsOfUser method updated

--- web/src/test/java/co/mobileaction/example/web/service/PostServiceTests.java
+++ web/src/test/java/co/mobileaction/example/web/service/PostServiceTests.java
@@ -5,7 +5,6 @@ import co.mobileaction.example.web.repository.IPostRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.jdbc.Sql;
@@ -79,8 +78,7 @@ public class PostServiceTests
     @Test
     public void deleteAllPostsOfUser()
     {
-        postService.deleteAllPostsByUserId(1L);
-        List<Post> list = postRepository.findAll();
+        List<Post> list = postRepository.findAllByUserId(1L);
         assertThat(list).hasSize(2);
     }
 }
