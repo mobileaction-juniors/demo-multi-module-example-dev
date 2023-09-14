1245a30d0b9291b956a26caa2a8acd6cc73de174
aycaaakyol <ayca.akyol@mobileaction.co>
2023-09-14 09:08:58 +0300

    feat(ONB-105): new docker-compose file

--- web/src/main/java/co/mobileaction/example/web/WebApplicationConfig.java
+++ web/src/main/java/co/mobileaction/example/web/WebApplicationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author sa
