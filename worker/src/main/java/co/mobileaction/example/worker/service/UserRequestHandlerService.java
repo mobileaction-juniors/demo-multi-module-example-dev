b2e03aad307854403470d9878e7fa150847b4922
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-08-19 15:50:20 +0300

    Tests for first task are added. ONB-202 is done.

---
+++ worker/src/main/java/co/mobileaction/example/worker/service/UserRequestHandlerService.java
@@ -0,0 +1,26 @@
+package co.mobileaction.example.worker.service;
+
+import co.mobileaction.example.common.dto.UserDto;
+import co.mobileaction.example.common.dto.UserQueueRequestDto;
+import co.mobileaction.example.worker.client.ICrawlerClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRequestHandlerService implements IUserRequestHandlerService {
+
+
+    private final AmqpTemplate userResultQueueTemplate;
+
+    private final ICrawlerClient crawlerClient;
+
+    @Override
+    public void executeMessage(UserQueueRequestDto request) {
+        UserDto user = crawlerClient.fetchUser(request.getId());
+
+        userResultQueueTemplate.convertAndSend(user);
+    }
+}
