b8884dc794fb314768028331f9d8a01b936c0697
mehmetserhatcelik <mehmetserhat86895@gmail.com>
2025-07-21 11:44:49 +0300

    feat(ONB-102): enhance QueueRequestDto and introduce UserDto for user management

--- common/src/main/java/co/mobileaction/example/common/dto/QueueRequestDto.java
+++ common/src/main/java/co/mobileaction/example/common/dto/QueueRequestDto.java
@@ -3,6 +3,7 @@ package co.mobileaction.example.common.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 /**
  * @author sa
@@ -12,7 +13,20 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class QueueRequestDto
-{
+@Builder
+public class QueueRequestDto {
     private Long postId;
+    private Long userId;
+
+    public QueueRequestDto(Long postId) {
+        this.postId = postId;
+    }
+
+    public static QueueRequestDto forUser(Long userId) {
+        return QueueRequestDto.builder().userId(userId).build();
+    }
+
+    public static QueueRequestDto forPost(Long postId) {
+        return QueueRequestDto.builder().postId(postId).build();
+    }
 }
