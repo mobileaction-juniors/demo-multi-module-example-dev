896e621cd95e71cc7049061f5ca2f3b2493ba864
Sezer Tetik <sezer.tetik2005@gmail.com>
2025-08-12 16:36:57 +0300

    fix(ONB-101): Transactional boundry added for deleteAllUserPosts

--- web/src/main/java/co/mobileaction/example/web/service/PostService.java
+++ web/src/main/java/co/mobileaction/example/web/service/PostService.java
@@ -2,6 +2,7 @@ package co.mobileaction.example.web.service;
 
 import co.mobileaction.example.web.model.Post;
 import co.mobileaction.example.web.repository.IPostRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -43,6 +44,7 @@ public class PostService implements IPostService
         postRepository.deleteById(postId);
     }
 
+    @Transactional
     @Override
     public void deleteAllUserPosts(Long userId) {
         postRepository.deleteAllByUserId(userId);
