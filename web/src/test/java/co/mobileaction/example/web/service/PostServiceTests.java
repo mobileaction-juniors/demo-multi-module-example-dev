433cbd87c915a84e2c0475503a8a2c54842cb23d
Okay Hamamcı <okayardah@gmail.com>
2026-06-12 12:03:47 +0300

    test: service layer test is added to PostServiceTests.java
    Service layer test for delete all functionality is implemented.

--- web/src/test/java/co/mobileaction/example/web/service/PostServiceTests.java
+++ web/src/test/java/co/mobileaction/example/web/service/PostServiceTests.java
@@ -74,4 +74,13 @@ public class PostServiceTests
 
         assertThat(list).hasSize(3);
     }
+    @Test
+    public void deleteAllPostsOfUser()
+    {
+        postService.deleteAllPostsOfUser(1L);
+
+        List<Post> list = postRepository.findAll();
+
+        assertThat(list).hasSize(2);
+    }
 }
