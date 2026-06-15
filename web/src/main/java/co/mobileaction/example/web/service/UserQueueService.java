5a1e3f58ade78d4ca66e7b52433708bb7e2446b4
Okay Hamamcı <okayardah@gmail.com>
2026-06-15 15:00:17 +0300

    feat: Admin side of the user queue is implemented for ONB-102

---
+++ web/src/main/java/co/mobileaction/example/web/service/UserQueueService.java
@@ -0,0 +1,26 @@
+package co.mobileaction.example.web.service;
+
+import co.mobileaction.example.common.dto.UserQueueRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueueService implements IUserQueueService
+{
+    private final AmqpTemplate requestQueueTemplate;
+    private final IPostService postService;
+
+    @Override
+    public void sendUserRequestForAllItems()
+    {
+        List<Long> distinctUserIds = postService.findDistinctUserIds();
+        
+        distinctUserIds.stream()
+                .map(UserQueueRequestDto::new)
+                .forEach(requestQueueTemplate::convertAndSend);
+    }
+}
