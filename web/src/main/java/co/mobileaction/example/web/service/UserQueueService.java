b06dfac16dc2e08e658a4aceebebb1b4647ea246
berkturk <berk.turk@mobileaction.co>
2026-06-23 14:20:51 +0300

    feat(ONB-102): Change sendUserRequestForDistinctItems to use distinct userIds

--- web/src/main/java/co/mobileaction/example/web/service/UserQueueService.java
+++ web/src/main/java/co/mobileaction/example/web/service/UserQueueService.java
@@ -1,12 +1,9 @@
 package co.mobileaction.example.web.service;
 
-import co.mobileaction.example.common.dto.QueueRequestDto;
 import co.mobileaction.example.common.dto.UserQueueRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.stream.LongStream;
 
 /**
  * @author berkturk
@@ -19,11 +16,13 @@ public class UserQueueService implements IUserQueueService
 {
     private final AmqpTemplate requestQueueTemplate;
 
+    private final PostService postService;
+
     @Override
     public void sendUserRequestForDistinctItems()
     {
-        LongStream.rangeClosed(1, 100)
-                .mapToObj(UserQueueRequestDto::new)
+        postService.findDistinctUserIds().stream()
+                .map(UserQueueRequestDto::new)
                 .forEach(requestQueueTemplate::convertAndSend);
     }
 }
