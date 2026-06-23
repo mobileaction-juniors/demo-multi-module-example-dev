2ac8601d93ec6b5c32ca80ff3b4e2978762f47e0
berkturk <berk.turk@mobileaction.co>
2026-06-23 09:01:49 +0300

    feat(ONB-101): Implement endpoint to delete all posts of a user

--- web/src/test/java/co/mobileaction/example/web/service/PostServiceTests.java
+++ web/src/test/java/co/mobileaction/example/web/service/PostServiceTests.java
@@ -74,4 +74,14 @@ public class PostServiceTests
 
         assertThat(list).hasSize(3);
     }
+
+    @Test
+    public void deleteAllPostsOfUser()
+    {
+        postService.deleteAllPostsOfUser(1L);
+
+        List<Post> list = postService.findAllPostsOfUser(1L);
+
+        assertThat(list).hasSize(0);
+    }
 }
