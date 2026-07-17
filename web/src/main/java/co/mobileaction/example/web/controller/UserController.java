d08309f5ea3d2566289beb659f779857bbf72590
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-17 15:58:35 +0300

    feat(post): add method findDistinctUsers method to find distinct users has posts

--- web/src/main/java/co/mobileaction/example/web/controller/UserController.java
+++ web/src/main/java/co/mobileaction/example/web/controller/UserController.java
@@ -1,7 +1,6 @@
 package co.mobileaction.example.web.controller;
 
 import co.mobileaction.example.web.util.SecurityUtils;
-import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
--- web/src/main/java/co/mobileaction/example/web/service/IPostService.java
+++ web/src/main/java/co/mobileaction/example/web/service/IPostService.java
@@ -21,4 +21,6 @@ public interface IPostService
     void deletePost(Long postId);
 
     boolean deleteAllPostsOfUser(Long userId);
+
+    List<Long> findDistinctUsers();
 }
