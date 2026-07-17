568721324271e3fcd96cabac978de31fc6a3fc4b
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-17 15:39:12 +0300

    feat(user): create LocalUser file for user entity

---
+++ web/src/main/java/co/mobileaction/example/web/model/LocalUser.java
@@ -0,0 +1,33 @@
+package co.mobileaction.example.web.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LocalUser
+{
+    @Id
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
