528272f75e6307e15f6f3e47c99b0c7c210af376
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-20 12:15:18 +0300

    test(user): add saveUser test to test saveUser method in UserResultHandlerService

--- common/src/main/java/co/mobileaction/example/common/dto/UserDto.java
+++ common/src/main/java/co/mobileaction/example/common/dto/UserDto.java
@@ -1,14 +1,12 @@
 package co.mobileaction.example.common.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class UserDto
 {
     private Long id;
