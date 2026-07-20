fdfbb371153404bab989cddd251d9f7327af29e2
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-20 11:52:39 +0300

    test(user): create users.sql file with sample data for test userService

---
+++ web/src/test/resources/data/users.sql
@@ -0,0 +1,4 @@
+insert into users(id, username, email, phone) values (1, 'user1', 'user1@gmail.com', '5000000001');
+insert into users(id, username, email, phone) values (2, 'user2', 'user2@gmail.com', '5000000002');
+insert into users(id, username, email, phone) values (3, 'user3', 'user3@gmail.com', '5000000003');
+insert into users(id, username, email, phone) values (4, 'user4', 'user4@gmail.com', '5000000004');
