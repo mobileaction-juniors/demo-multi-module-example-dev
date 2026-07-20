c89e781a02c64a342b0de783b94a2d7aed7fee01
kursathurkanaydin <kursat.aydin@hacettepe.edu.tr>
2026-07-20 13:35:51 +0300

    test(user): add test for UserRequestHandlerService

---
+++ worker/src/test/java/co/mobileaction/example/worker/service/UserRequestHandlerServiceTests.java
@@ -0,0 +1,43 @@
+package co.mobileaction.example.worker.service;
+
+import co.mobileaction.example.common.dto.UserDto;
+import co.mobileaction.example.common.dto.UserQueueRequestDto;
+import co.mobileaction.example.worker.client.ICrawlerClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.core.AmqpTemplate;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserRequestHandlerServiceTests
+{
+    @InjectMocks
+    private UserRequestHandlerService requestHandlerService;
+
+    @Mock
+    private ICrawlerClient crawlerClient;
+
+    @Mock
+    private AmqpTemplate userResultQueueTemplate;
+
+    @Test
+    public void test_executeMessage_fetchesUserAndSendsResult()
+    {
+        UserDto user = new UserDto(1L, "Bret", "Sincere@april.biz", "1-770-736-8031 x56442");
+
+        when(crawlerClient.fetchUser(eq(1L))).thenReturn(user);
+
+        UserQueueRequestDto dto = new UserQueueRequestDto(1L);
+
+        requestHandlerService.executeMessage(dto);
+
+        verify(crawlerClient).fetchUser(1L);
+        verify(userResultQueueTemplate).convertAndSend(user);
+    }
+}
